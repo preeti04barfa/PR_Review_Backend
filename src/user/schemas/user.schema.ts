3cfc8ae837e5a9253319bb04da6cf89f5ac8e09a
preetibarfa.vhits@gmail.com <preetibarfa.vhits@gmail.com>
2025-07-14 11:54:06 +0530

    workin on PR view

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,7 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 
-// 👇 Use HydratedDocument instead of Document for better type support
 export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true }) 
